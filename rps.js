cd7485fa5d368fcd010ee7551670df3fca784dc4
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-20 01:27:03 +0000

    Revisions for round functionality to spec
    Added winner messages to rounds and game

--- rps.js
+++ rps.js
@@ -1,7 +1,5 @@
 let humanScore = 0;
 let computerScore = 0;
-let humanChoice = toLowerCase(getHumanChoice());
-let computerChoice = getComputerChoice();
 
 function getComputerChoice() {
     return Math.random();
@@ -11,54 +9,85 @@ function getHumanChoice() {
    return String(window.prompt("Rock, Paper, Scissors?", ""));
 }
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+    let i = 0;
+    let message = "";
+    function playRound(humanChoice, computerChoice) {
 
     if (humanChoice = "rock") {
         if (computerChoice < 0.34) {
+            message = "tie!"
+            console.log(message)
             return;
         }
         else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "you lose! paper beats rock"
+            console.log(message)
             computerScore ++;
             return;
         }
         else if (computerChoice >= 0.67) {
+            message = "you win! rock beats scissors"
+            console.log(message)
             humanScore ++;
             return;
         }
     }
     else if (humanChoice = "paper") {
         if (computerChoice < 0.34) {
+            message = "you win! paper beats rock"
+            console.log(message)
             humanScore ++;
             return;        
         }
         else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "tie!"
+            console.log(message)
             return;
         }
         else if (computerChoice >= 0.67) {
+            message = "you lose! scissors beats paper"
+            console.log(message)
             computerScore ++;
             return;
         }
     }
     else if (humanChoice = "scissors") {
         if (computerChoice < 0.34) {
+            message = "you lose! rock beats scissors"
+            console.log(message)
             computerScore ++;
             return;        
         }
         else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "you win! scissors beats paper"
+            console.log(message)
             humanScore ++;
             return;
         }
         else if (computerChoice >= 0.67) {
+            message = "tie!"
+            console.log(message)
             return;
         }
     }
 }
-
-function playGame() {
-    let i = 0;
     while (i < 5) {
+        let humanChoice = getHumanChoice().toLowerCase();
+        let computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice)
+        console.log(humanScore, computerScore)
         i ++;
     }
-    return humanScore, computerScore;
+    let winner = ""
+    if (humanScore > computerScore) {
+        winner = "you win!"
+    }
+    else {
+        winner = "you lose!"
+    }
+    console.log(winner)
+    return;
 }
+
+playGame()
