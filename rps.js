0e263dfe3feb002b17f39f3fc706dbe5f4f761a9
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-22 02:10:42 +0000

    Updated game logic and button text
    Added function for displaying game scores and adjusted game logic

--- rps.js
+++ rps.js
@@ -27,85 +27,92 @@ buttons.addEventListener('click'), (event) => {
     }
 };
 
-// function playGame() {
-//     let i = 0;
-//     let message = "";
-//     function playRound(humanChoice, computerChoice) {
+function playGame() {
+    let i = 0;
+    let message = "";
+    function playRound(humanChoice, computerChoice) {
 
-//     if (humanChoice = "rock") {
-//         if (computerChoice < 0.34) {
-//             message = "tie!"
-//             console.log(message)
-//             return;
-//         }
-//         else if (computerChoice > 0.33 && computerChoice < 0.66) {
-//             message = "you lose! paper beats rock"
-//             console.log(message)
-//             computerScore ++;
-//             return;
-//         }
-//         else if (computerChoice >= 0.67) {
-//             message = "you win! rock beats scissors"
-//             console.log(message)
-//             humanScore ++;
-//             return;
-//         }
-//     }
-//     else if (humanChoice = "paper") {
-//         if (computerChoice < 0.34) {
-//             message = "you win! paper beats rock"
-//             console.log(message)
-//             humanScore ++;
-//             return;        
-//         }
-//         else if (computerChoice > 0.33 && computerChoice < 0.66) {
-//             message = "tie!"
-//             console.log(message)
-//             return;
-//         }
-//         else if (computerChoice >= 0.67) {
-//             message = "you lose! scissors beats paper"
-//             console.log(message)
-//             computerScore ++;
-//             return;
-//         }
-//     }
-//     else if (humanChoice = "scissors") {
-//         if (computerChoice < 0.34) {
-//             message = "you lose! rock beats scissors"
-//             console.log(message)
-//             computerScore ++;
-//             return;        
-//         }
-//         else if (computerChoice > 0.33 && computerChoice < 0.66) {
-//             message = "you win! scissors beats paper"
-//             console.log(message)
-//             humanScore ++;
-//             return;
-//         }
-//         else if (computerChoice >= 0.67) {
-//             message = "tie!"
-//             console.log(message)
-//             return;
-//         }
-//     }
-// }
-//     while (i < 5) {
-//         let humanChoice = getHumanChoice().toLowerCase();
-//         let computerChoice = getComputerChoice();
-//         playRound(humanChoice, computerChoice)
-//         console.log(humanScore, computerScore)
-//         i ++;
-//     }
-//     let winner = ""
-//     if (humanScore > computerScore) {
-//         winner = "you win!"
-//     }
-//     else {
-//         winner = "you lose!"
-//     }
-//     console.log(winner)
-//     return;
-// }
+    if (humanChoice = "rock") {
+        if (computerChoice < 0.34) {
+            message = "tie!"
+            console.log(message)
+            return;
+        }
+        else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "you lose! paper beats rock"
+            console.log(message)
+            computerScore ++;
+            return;
+        }
+        else if (computerChoice >= 0.67) {
+            message = "you win! rock beats scissors"
+            console.log(message)
+            humanScore ++;
+            return;
+        }
+    }
+    else if (humanChoice = "paper") {
+        if (computerChoice < 0.34) {
+            message = "you win! paper beats rock"
+            console.log(message)
+            humanScore ++;
+            return;        
+        }
+        else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "tie!"
+            console.log(message)
+            return;
+        }
+        else if (computerChoice >= 0.67) {
+            message = "you lose! scissors beats paper"
+            console.log(message)
+            computerScore ++;
+            return;
+        }
+    }
+    else if (humanChoice = "scissors") {
+        if (computerChoice < 0.34) {
+            message = "you lose! rock beats scissors"
+            console.log(message)
+            computerScore ++;
+            return;        
+        }
+        else if (computerChoice > 0.33 && computerChoice < 0.66) {
+            message = "you win! scissors beats paper"
+            console.log(message)
+            humanScore ++;
+            return;
+        }
+        else if (computerChoice >= 0.67) {
+            message = "tie!"
+            console.log(message)
+            return;
+        }
+    }
+}
+    while (i < 5) {
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice)
+        displayScore(humanScore, computerScore)
+        i ++;
+    }
+    let winner = ""
+    if (humanScore > computerScore) {
+        winner = "you win!"
+    }
+    else {
+        winner = "you lose!"
+    }
+    console.log(winner)
+    return;
+}
 
-// playGame()
+function displayScore (humanScore, computerScore) {
+    let hScoreDisplay = document.getElementById('hScore');
+    let cScoreDisplay = document.getElementById('cScore');
+
+    hScoreDisplay.innerText = `Human Score: ${humanScore}`;
+    cScoreDisplay.innerText = `Computer Score: ${computerScore}`;
+}
+
+playGame()
