2765078fe37ce637aebe810bb66e170d675249da
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-23 12:57:46 +1200

    Refactored code to not have seperate game function
    Deleted game logic and called logic functions individually
    Also renamed computerChoice logic to make it easier to read

--- rps.js
+++ rps.js
@@ -1,15 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
+let computerChoice = 0;
 let humanChoice = "";
 let message = "";
-
-function getComputerChoice() {
-    return Math.random();
-}
-
-function getHumanChoice() {
-   return String(window.prompt("Rock, Paper, Scissors?", ""));
-}
+let i = 0;
 
 let buttons = document.getElementById('controlButtons');
 
@@ -18,108 +12,150 @@ buttons.addEventListener('click', (event) => {
 
     switch(target.id) {
         case 'Rock':
-            humanChoice = target.id.toLowerCase();
-            playGame();
+            if (i < 5) {
+                humanChoice = target.id.toLowerCase();
+                computerChoice = getComputerChoice();
+                console.log(humanChoice, computerChoice, i)
+                playRound(humanChoice, computerChoice);
+                displayScore (humanScore, computerScore, message);
+                i++;
+                break;
+            }
+            winnerLogic(humanScore, computerScore);
+            displayWinner (winner)
             break;
         case 'Paper':
-            humanChoice = target.id.toLowerCase();
-            playGame();
+            if (i < 5) {
+                humanChoice = target.id.toLowerCase();
+                computerChoice = getComputerChoice();
+                console.log(humanChoice, computerChoice)
+                playRound(humanChoice, computerChoice);
+                displayScore (humanScore, computerScore, message);
+                i++;
+                break;
+            }
+            winnerLogic(humanScore, computerScore);
+            displayWinner (winner)
             break;
         case "Scissors":
-            humanChoice = target.id.toLowerCase();
-            playGame();
+            if (i < 5) {
+                humanChoice = target.id.toLowerCase();
+                computerChoice = getComputerChoice();
+                console.log(humanChoice, computerChoice)
+                playRound(humanChoice, computerChoice);
+                displayScore (humanScore, computerScore, message);
+                i++;
+                break;
+            }
+            winnerLogic(humanScore, computerScore);
+            displayWinner (winner)
             break;
     }
 });
+
+function winnerLogic(humanScore, computerScore){
+    if (humanScore > computerScore) {
+        winner = "you win the game!"
+    }
+    else {
+        winner = "you lose the game!"
+    }
+};
+
+
+function gameLogic() {
+    let i = 0;
+    
+    
+}
+function getComputerChoice() {
+    computerChoice = Math.random()
+    
+    switch(computerChoice) {
+        case (computerChoice < 0.34):
+            computerChoice = "rock"
+            break;
+        case (computerChoice > 0.34 && computerChoice < 0.66):
+            computerChoice = "paper"
+            break;
+        case (computerChoice >= 0.66):
+            computerChoice = "scissors"
+    };
+}
+
+
 function playRound(humanChoice, computerChoice) {
     console.log(humanChoice)
-    if (humanChoice = "rock") {
-        if (computerChoice < 0.34) {
-            message = "tie!"
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            message = "tie!";
             return;
         }
-        else if (computerChoice > 0.33 && computerChoice < 0.66) {
-            message = "you lose! paper beats rock"
+        else if (computerChoice === "paper") {
+            message = "you lose! paper beats rock";
             computerScore ++;
             return;
         }
-        else if (computerChoice >= 0.67) {
-            message = "you win! rock beats scissors"
+        else {
+            message = "you win! rock beats scissors";
             humanScore ++;
             return;
         }
     }
-    else if (humanChoice = "paper") {
-        if (computerChoice < 0.34) {
-            message = "you win! paper beats rock"
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            message = "you win! paper beats rock";
             humanScore ++;
             return;        
         }
-        else if (computerChoice > 0.33 && computerChoice < 0.66) {
+        else if (computerChoice === "paper") {
             message = "tie!"
             return;
         }
-        else if (computerChoice >= 0.67) {
-            message = "you lose! scissors beats paper"
+        else {
+            message = "you lose! scissors beats paper";
             computerScore ++;
             return;
         }
     }
-    else if (humanChoice = "scissors") {
-        if (computerChoice < 0.34) {
-            message = "you lose! rock beats scissors"
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            message = "you lose! rock beats scissors";
             computerScore ++;
             return;        
         }
-        else if (computerChoice > 0.33 && computerChoice < 0.66) {
-            message = "you win! scissors beats paper"
+        else if (computerChoice === "paper") {
+            message = "you win! scissors beats paper";
             humanScore ++;
             return;
         }
-        else if (computerChoice >= 0.67) {
-            message = "tie!"
+        else {
+            message = "tie!";
             return;
         }
     }
 };
 
 
-function displayScore (humanScore, computerScore, message) {
+function displayScore (humanScore, computerScore, message, computerChoice) {
+    let hScoreDisplay = document.getElementById('hScore');
+    let cScoreDisplay = document.getElementById('cScore');
+    let cChoiceDisplay = document.getElementById('cSelection');
+    let results = document.getElementById('results');
+
+    cChoiceDisplay = `Computer Choice: ${computerChoice}`;
+    hScoreDisplay.innerText = `Human Score: ${humanScore}`;
+    cScoreDisplay.innerText = `Computer Score: ${computerScore}`;
+    results.innerText = `${message}`;
+}
+
+function displayWinner (winner, message) {
     let hScoreDisplay = document.getElementById('hScore');
     let cScoreDisplay = document.getElementById('cScore');
     let results = document.getElementById('results');
 
     hScoreDisplay.innerText = `Human Score: ${humanScore}`;
     cScoreDisplay.innerText = `Computer Score: ${computerScore}`;
-    results.innerText = `${message}`;
+    results.innerText = `${message}, ${winner}`;
 }
 
-function displayWinner (winner) {
-    let hScoreDisplay = document.getElementById('hScore');
-    let cScoreDisplay = document.getElementById('cScore');
-    let results = document.getElementById('results');
-
-    hScoreDisplay.innerText = "";
-    cScoreDisplay.innerText = "";
-    results.innerText = `${winner}`;
-}
-function playGame() {
-    let i = 0;
-    while (i < 5) {
-            let computerChoice = getComputerChoice();
-            playRound(humanChoice, computerChoice)
-            displayScore(humanScore, computerScore, message)
-            i++
-            return;
-        }
-        let winner = ""
-    if (humanScore > computerScore) {
-        winner = "you win!"
-    }
-    else {
-        winner = "you lose!"
-    }
-    displayWinner(winner);
-    return;
-}
-playGame()
